8c37d3cb7114d6dc3cbc5b308886de6edc7e0e17
gabrielchen9527 <gabrielchen@gwu.edu>
2016-11-25 17:11:54 -0500

    detele all

--- README.md
+++
@@ -1,1 +0,0 @@
-If any issue occur in faceRec, can replace movie to face, just modify the lower and upper bound in main m file. nothing else is needed.
--- simulation/faceRecMat/run.m
+++
@@ -1,6 +0,0 @@
-clear;
-W=20;R=0.88;alpha=0.5;
-[workload,precision,energy,latency,result]=gaScheduling(R,W,alpha);
-[workload,precision,energy,latency,result]=even(R,W,alpha);
-[workload,precision,energy,latency,result]=inverse(R,W,alpha);
-[workload,precision,energy,latency,result]=loadbalance(R,W,alpha);
